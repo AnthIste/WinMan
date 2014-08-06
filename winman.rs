ebb4080a4df97748697d445df02a6e370c81366a
ruben.bakker <ruben.bakker@dariel.co.za>
2014-08-06 18:07:45 +0200

    Start refactoring to structs and traits

--- winman.rs
+++ winman.rs
@@ -11,7 +11,7 @@ use win32::types::{HWND,MSG,UINT,DWORD,WORD,NOTIFYICONDATA,WNDCLASSEX,WNDPROC,WP
 use win32::window::{MessageBoxA,GetMessageW,TranslateMessage,DispatchMessageW,RegisterHotKey,PostQuitMessage,
                     Shell_NotifyIcon,RegisterClassExW,DefWindowProcW,CreateWindowExW,GetLastError,LoadImageW,
                     GetSystemMetrics,GetModuleHandleW,CreatePopupMenu,AppendMenuA,GetCursorPos,
-                    SetForegroundWindow,TrackPopupMenu};
+                    SetForegroundWindow,TrackPopupMenu,DestroyWindow};
 use win32::wstr::ToCWStr;
 
 // Consider moving to crate win32
@@ -27,64 +27,6 @@ static MOD_SWITCH: UINT = MOD_ALT;
 #[allow(non_snake_case_functions)]
 fn MAKEINTRESOURCEW(i: UINT) -> LPCWSTR {
     ((i as WORD) as ULONG_PTR) as LPCWSTR
-}
-
-fn create_dummy_window(wndProc: WNDPROC) -> Result<HWND, DWORD> {
-    let mut wc: WNDCLASSEX = Default::default();
-
-    wc.lpfnWndProc = wndProc;
-    wc.lpszClassName = "MyMagicClassName".to_c_wstr().as_ptr();
-
-    if RegisterClassExW(&wc) == 0 {
-        return Err(GetLastError());
-    }
-
-    let hWnd = CreateWindowExW(
-        0,
-        "MyMagicClassName".to_c_wstr().as_ptr(),
-        0 as LPCWSTR,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0 as HWND,
-        0 as HMENU,
-        0 as HINSTANCE,
-        0 as LPVOID);
-
-    if hWnd == 0 as HWND {
-        Err(GetLastError())
-    }
-    else {
-        Ok(hWnd)
-    }
-}
-
-fn register_systray_icon(hWnd: HWND) -> NOTIFYICONDATA {
-    let hInstance = GetModuleHandleW(0 as LPCWSTR);
-    let mut nid: NOTIFYICONDATA = Default::default();
-
-    nid.uID = 0x29A;
-    nid.uCallbackMessage = 1234;
-    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-    nid.hWnd = hWnd;
-    nid.hIcon = LoadImageW(
-        hInstance,
-        MAKEINTRESOURCEW(IDI_ICON1),
-        1, // IMAGE_ICON
-        GetSystemMetrics(49), // SM_CXSMICON
-        GetSystemMetrics(50), // SM_CYSMICON
-        0 // LR_DEFAULTCOLOR
-        ) as HICON;
-
-    Shell_NotifyIcon(NIM_ADD, &mut nid);
-
-    nid
-}
-
-fn deregister_systray_icon(nid: &mut NOTIFYICONDATA) {
-    Shell_NotifyIcon(NIM_DELETE, nid);
 }
 
 fn show_popup_menu(hWnd: HWND) {
@@ -146,49 +88,150 @@ fn process_hotkey(hotkey: (UINT, UINT)) {
     }
 }
 
-fn loword(u: DWORD) -> WORD {
+fn hiword(u: DWORD) -> WORD {
     ((u & 0xFFFF0000) >> 16) as WORD
 }
 
-fn hiword(u: DWORD) -> WORD {
+fn loword(u: DWORD) -> WORD {
     (u & 0x0000FFFF) as WORD
 }
 
 fn extract_hotkey(msg: &MSG) -> (UINT, UINT) {
-    let modifiers = hiword(msg.lParam as DWORD) as UINT;
-    let vk = loword(msg.lParam as DWORD) as UINT;
+    let modifiers = loword(msg.lParam as DWORD) as UINT;
+    let vk = hiword(msg.lParam as DWORD) as UINT;
 
     (modifiers, vk)
 }
+
+struct DummyWindow {
+    pub hWnd: HWND,
+    pub nid: Option<NOTIFYICONDATA>,
+}
+
+impl DummyWindow {
+    pub fn create(wndProc: WNDPROC) -> Result<DummyWindow, u32> {
+        let mut wc: WNDCLASSEX = Default::default();
+
+        wc.lpfnWndProc = wndProc;
+        wc.lpszClassName = "MyMagicClassName".to_c_wstr().as_ptr();
+
+        if RegisterClassExW(&wc) == 0 {
+            return Err(GetLastError());
+        }
+
+        let hWnd = CreateWindowExW(
+            0,
+            "MyMagicClassName".to_c_wstr().as_ptr(),
+            0 as LPCWSTR,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0 as HWND,
+            0 as HMENU,
+            0 as HINSTANCE,
+            0 as LPVOID);
+
+        if hWnd == 0 as HWND {
+            return Err(GetLastError());
+        }
+
+        let dummy_window = DummyWindow {
+            hWnd: hWnd,
+            nid: None,
+        };
+
+        Ok(dummy_window)
+    }
+
+    pub fn register_systray_icon(&mut self) {
+        match self.nid {
+            None => {
+                let hInstance = GetModuleHandleW(0 as LPCWSTR);
+                let mut nid: NOTIFYICONDATA = Default::default();
+
+                nid.uID = 0x29A;
+                nid.uCallbackMessage = 1234;
+                nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+                nid.hWnd = self.hWnd;
+                nid.hIcon = LoadImageW(
+                    hInstance,
+                    MAKEINTRESOURCEW(IDI_ICON1),
+                    1, // IMAGE_ICON
+                    GetSystemMetrics(49), // SM_CXSMICON
+                    GetSystemMetrics(50), // SM_CYSMICON
+                    0 // LR_DEFAULTCOLOR
+                    ) as HICON;
+
+                Shell_NotifyIcon(NIM_ADD, &mut nid);
+
+                self.nid = Some(nid);
+            }
+            Some(_) => { }
+        }
+    }
+
+    pub fn deregister_systray_icon(&mut self) {
+        match self.nid {
+            Some(mut nid) => {
+                Shell_NotifyIcon(NIM_DELETE, &mut nid);
+                self.nid = None;
+            }
+            None => { }
+        }
+    }
+}
+
+trait OnWindowMessage {
+    fn on_create(&mut self) -> bool { true }
+
+    fn on_destroy(&mut self) -> bool { true }
+}
+
+impl OnWindowMessage for DummyWindow {
+    fn on_create(&mut self) -> bool {
+        self.register_systray_icon();
+        true
+    }
+
+    fn on_destroy(&mut self) -> bool {
+        PostQuitMessage(0);
+        true
+    }
+}
+
+// static mut s_win: Option<&DummyWindow> = None;
 
 fn main() {
     // https://github.com/rust-lang/rust/issues/13259
     unsafe { ::std::rt::stack::record_sp_limit(0); }
 
-    let hWndResult = create_dummy_window(main_wnd_proc as WNDPROC);
-
     // Potential macro to handle Option<T> failures:
     // `macro_rules! try_option {($x:expr) => (match $x {Some(x) => x, None => return})}`
     // Otherwise use try! with Result<T, E>
 
-    match hWndResult {
-        Ok(hWnd) => {
-            register_hotkeys();
+    let create_result = DummyWindow::create(main_wnd_proc as WNDPROC);
 
-            let mut nid = register_systray_icon(hWnd);
+    match create_result {
+        Ok(mut dummy_window) => {
+            register_hotkeys();
+            dummy_window.register_systray_icon();
+
             let mut msg: MSG = Default::default();
 
             while GetMessageW(&mut msg, 0 as HWND, 0, 0) > 0 {
                 TranslateMessage(&mut msg);
                 DispatchMessageW(&mut msg);
 
+                // Hotkeys are sent to the thread, not the window
                 if msg.message == WM_HOTKEY {
                     let hotkey = extract_hotkey(&msg);
                     process_hotkey(hotkey);
                 }
             }
 
-            deregister_systray_icon(&mut nid);
+            dummy_window.deregister_systray_icon();
 
             MessageBoxA(0 as HWND, "All done!".to_c_str().as_ptr(), "Exiting".to_c_str().as_ptr(), 0);
         }
@@ -200,10 +243,35 @@ fn main() {
 }
 
 extern "system" fn main_wnd_proc(hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM) -> LRESULT {
-    if msg == 1234 {
-        show_popup_menu(hWnd);
-        return 0 as LRESULT;
+    match msg {
+        1234 => {
+            match lParam as UINT {
+                WM_LBUTTONDBLCLK => {
+                    DestroyWindow(hWnd);
+                }
+                WM_RBUTTONDOWN | WM_CONTEXTMENU => {
+                    show_popup_menu(hWnd);
+                }
+                _ => { }
+            }
+        }
+
+        WM_COMMAND => {
+            match loword(wParam as DWORD) {
+                1 => { // TM_EXIT
+                    DestroyWindow(hWnd);
+                }
+                _ => { }
+            }
+        }
+
+        WM_DESTROY => {
+        }
+
+        _ => {
+            return DefWindowProcW(hWnd, msg, wParam, lParam);
+        }
     }
 
-    DefWindowProcW(hWnd, msg, wParam, lParam)
+    0 as LRESULT
 }
