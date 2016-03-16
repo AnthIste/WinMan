6c34b80fad88c42a2882e348550f837de9ad8a51
Ruben Bakker <ruben.bakker@dariel.co.za>
2016-03-16 21:07:53 +0200

    Basic hide-show on foreground window

--- src/winman.rs
+++ src/winman.rs
@@ -1,3 +1,19 @@
+extern crate winapi;
+extern crate kernel32;
+extern crate user32;
+
+use winapi::winuser::*;
+
 pub fn main() {
 	println!("Hello Windows!");
+
+	let foreground_window = unsafe { user32::GetForegroundWindow() };
+
+	println!("{:?}", foreground_window);
+
+	unsafe {
+		user32::ShowWindow(foreground_window, SW_HIDE);
+		kernel32::Sleep(1000);
+		user32::ShowWindow(foreground_window, SW_SHOW);
+	}
 }
