c9823ca9f5f98f29d9c7cebe8ad395c1cad9c917
Ruben Bakker <ruben.bakker@dariel.co.za>
2016-03-16 21:14:49 +0200

    Add example WindowProc

--- src/winman.rs
+++ src/winman.rs
@@ -2,6 +2,8 @@ extern crate winapi;
 extern crate kernel32;
 extern crate user32;
 
+use winapi::minwindef::*;
+use winapi::windef::*;
 use winapi::winuser::*;
 
 pub fn main() {
@@ -16,4 +18,18 @@ pub fn main() {
 		kernel32::Sleep(1000);
 		user32::ShowWindow(foreground_window, SW_SHOW);
 	}
+}
+
+extern "system" fn _window_proc(hwnd: HWND, msg: UINT, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    unsafe {
+        let lresult = match msg {
+            WM_CREATE               => Some(0),
+            WM_DESTROY              => Some(0),
+            WM_COMMAND              => Some(0),
+            user if user >= WM_USER => Some(0),
+            _                       => None
+        };
+
+        lresult.unwrap_or(user32::DefWindowProcW(hwnd, msg, wparam, lparam))
+    }
 }
