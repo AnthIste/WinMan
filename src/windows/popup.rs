83ef6355ae19fb32adf5497a458ef1ba0f5313f1
Ruben Bakker <rubenbak@gmail.com>
2017-04-17 13:31:47 +0200

    Revert "Move construction out of WM_CREATE"
    This reverts commit f59b64ebb2cc595268595e0af671beb16fcff3c2.

--- src/windows/popup.rs
+++ src/windows/popup.rs
@@ -171,19 +171,11 @@ pub fn create_window() -> Win32Result<PopupWindowShared> {
             0 as LPVOID)
     };
 
-    let instance = PopupWindow::new(hwnd);
+    let ref mut map = POPUP_INSTANCES.lock().unwrap();
 
-    match instance {
-        Ok(instance) => {
-            let ref mut map = POPUP_INSTANCES.lock().unwrap();
-            map.set(hwnd, Ok(instance));
-            Ok(map.get(hwnd).expect("Window was just created and an should exist"))
-        },
-
-        Err(e) => {
-            unsafe { user32::DestroyWindow(hwnd); }
-            Err(e)        
-        }
+    match map.get(hwnd) {
+        Some(shared) => Ok(shared),
+        None => Err(map.get_err().expect("Either window or err must be set after window creation"))
     }
 }
 
@@ -261,6 +253,19 @@ unsafe extern "system" fn window_proc(hwnd: HWND, msg: UINT, wparam: WPARAM, lpa
                 },
                 
                 _ => None
+            },
+
+            // Window creating
+            None if msg == WM_CREATE => {
+                let instance = PopupWindow::new(hwnd);
+                let lresult = match instance {
+                    Err(_) => -1,
+                    _ => 0,
+                };
+
+                map.set(hwnd, instance);
+
+                Some(lresult)
             },
 
             // Unknown window lifecycle
